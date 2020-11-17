15f7e00538bcf0aceb1dee848e43cf16b0731f4f
Max Ainatchi <maxwell.ainatchi@gmail.com>
2020-11-17 09:35:29 -0500

    Moved TODO to README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sorting-algorithm-tester
+
 Created with CodeSandbox
+
+# To Do
+
+- Bug: running tests too many times crashes the page
+  - Caused by success animation (temporarily disabled), figure out why
+- Add auto-save
+- Add a features list
+- UseReducer for better state management
+- Add dark mode to localstorage/cookie
+- Make more generic so it can use any problem set
+  - Use custom success logic
+  - Use arbitrary function names
+  - Use a problems folder to load supported problems
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,3 @@
-/**
- * TODO
- *
- * - Bug: running tests too many times crashes the page
- *   - Caused by success animation, figure out why
- * - Add auto-save
- * - Add a features list
- * - UseReducer for better state management
- * - Add dark mode to localstorage/cookie
- * - Make more generic so it can use any problem set
- *   - Use custom success logic
- *   - Use arbitrary function names
- *   - Use a problems folder to load supported problems
- */
-
 import React, { useState, useRef } from "react";
 import Test from "./Test";
 import "./styles.css";
